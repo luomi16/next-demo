fbaf9a1151a04604ab9b060bb686c184f48a4552
miluo16 <miluo@udel.edu>
2024-03-25 18:19:53 -0400

    Create Post Form

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains: ["images.pexels.com", "github.com"],
-    }
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.pexels.com',
+            // Optional: Specify a port if your external source uses a non-standard port
+            // port: '8080',
+            // Optional: Specify a pathname prefix if you want to match only images from a specific path
+            // pathname: '/images/*',
+          },
+          {
+            protocol: 'https',
+            hostname: 'github.com',
+          },
+        ],
+      },
 };
 
 export default nextConfig;

08f15d89a7e25a8942bd5ab47449040c85ae36cd
miluo16 <miluo@udel.edu>
2024-03-29 02:15:17 -0400

    Setting up prisma

--- next.config.mjs
+++ next.config.mjs
@@ -18,6 +18,11 @@ const nextConfig = {
             protocol: 'https',
             hostname: 'avatars.githubusercontent.com',
           },
+          {
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',
+          },
+          
         ],
       },
 };

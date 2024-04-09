6e4f4cdd7306378b77f85da94f3bb385c2bea23b
miluo16 <miluo@udel.edu>
2024-04-08 22:53:06 -0400

    Fetch posts data by author for dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,27 +1,49 @@
-import Post from "@/components/Post";
-import { postsData } from "@/data";
 import Link from "next/link";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+// import { Post } from "@prisma/client";
+import { TPost } from "@/app/page";
+import Post from "@/components/Post";
+
+// Get posts by author
+const getPosts = async (email: string) => { 
+  try {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/authors/${email}`);
+    // destructure posts from res
+    const {posts} = await res.json();
+    return posts
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
 
 export default async function Dashboard() {
   const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
+  let posts = [];
+
   if(!session) redirect("sign-in");
+
+  if(email) {
+    posts = await getPosts(email);
+  }
+
   return (
     <div>
       <h1>My posts</h1>
-      {postsData && postsData.length > 0 ? (
-        postsData.map((post) => (
+      {posts && posts.length > 0 ? (
+        posts.map((post: TPost) => (
           <Post
             key={post.id}
             id={post.id}
-            author={post.author}
-            authorEmail={"test@example.com"}
-            date={post.datepublished}
+            author={""}
+            authorEmail={post.authorEmail}
+            date={post.createdAt}
             title={post.title}
-            thumbnail={post.thumbnail}
-            category={post.category}
+            thumbnail={post.imageUrl}
+            category={post.catName}
             content={post.content}
             links={post.links || []}
           />

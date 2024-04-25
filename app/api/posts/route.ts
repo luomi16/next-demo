99cb055ac1d9ffe94e794012d27b42b76b12b826
miluo16 <miluo@udel.edu>
2024-04-25 01:21:56 -0400

    Try to optimize api/posts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -41,9 +41,34 @@ export async function POST(req: Request) {
   }
 }
 
+// export async function GET() {
+//   try {
+//     const posts = await prisma.post.findMany({
+//       include: { author: { select: { name: true } } },
+//       orderBy: {
+//         createdAt: 'desc',
+//       },
+//     });
+//     return NextResponse.json(posts);
+//   } catch (error) {
+//     console.log(error);
+//     return NextResponse.json(
+//       { message: 'Some error occured' },
+//       { status: 500 }
+//     );
+//   }
+// }
+
 export async function GET() {
+  // const page = parseInt(req.query.page) || 1; // 从查询参数获取页码，默认为第一页
+  const page = 1;
+  const limit = 10;
+  const skip = (page - 1) * limit;
+
   try {
     const posts = await prisma.post.findMany({
+      take: limit,
+      skip: skip,
       include: { author: { select: { name: true } } },
       orderBy: {
         createdAt: 'desc',
@@ -52,9 +77,7 @@ export async function GET() {
     return NextResponse.json(posts);
   } catch (error) {
     console.log(error);
-    return NextResponse.json(
-      { message: 'Some error occured' },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: 'Some error occurred' }, { status: 500 });
   }
 }
+
